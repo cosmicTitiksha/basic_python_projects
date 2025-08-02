c74b23427b632dfe5e4c8a54ea0416d4c3801dcb
Titiksha <t02437356@gmail.com>
2025-08-02 22:27:36 +0530

    Add files via upload
    At a beginner level, this CLI calculator:
    * Takes user input ✔️
    * Handles multiple operations ✔️
    * Uses a function ✔️
    * Includes a loop for repetition ✔️
    * Has basic error handling (like division by zero) ✔️

---
+++ calculator_app_CLI.py
@@ -0,0 +1,26 @@
+def calculation(operand1, operand2, operation):
+  if operation == '+':
+    print(f"So you want addition {operand1+operand2}")
+  elif operation == '-':
+    print(f"Okay subtraction {operand1-operand2}")
+  elif operation == '*':
+    print(f"Their product is {operand1*operand2}")
+  elif operation == '/':
+    if operand2 == 0:
+      print("Sorry, can't be divided by 0, your computer won't be able to hold that number!")
+    else:
+      print(f"The quotient is {operand1/operand2}")
+  else:
+    print(f"We are building this app for {operation} as well.")
+
+while True:
+    operand1 = float(input("Enter first number: "))
+    operand2 = float(input("Enter second number: "))
+    operation = input("Enter the operation (+, -, *, /): ")
+
+    calculation(operand1, operand2, operation)
+
+    iteration = input("Have more calculations to perform? (yes/no): ").lower()
+    if iteration != 'yes':
+        print("Okay, until next time!")
+        break
