cced686f9440e567163fd0ab0ed6dbf56b1a734f
Titiksha <t02437356@gmail.com>
2025-08-03 00:06:07 +0530

    calculator_app_CLI.py

--- calculator_app_CLI.py
+++ calculator_app_CLI.py
@@ -7,7 +7,7 @@ def calculation(operand1, operand2, operation):
     print(f"Their product is {operand1*operand2}")
   elif operation == '/':
     if operand2 == 0:
-      print("Sorry, can't be divided by 0, your computer won't be able to hold that number!")
+      print("Sorry, can't be divided by 0, No definite answer!")
     else:
       print(f"The quotient is {operand1/operand2}")
   else:
@@ -23,4 +23,5 @@ while True:
     iteration = input("Have more calculations to perform? (yes/no): ").lower()
     if iteration != 'yes':
         print("Okay, until next time!")
+
         break
